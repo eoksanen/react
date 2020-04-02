05b3ffd194e5a13c6d82365bee84096dffefa8b1
Ilmari <eoksanen@gmail.com>
2020-04-02 09:41:15 +0300

    2

--- part2/phonebook/App.js
+++ part2/phonebook/App.js
@@ -1,26 +1,57 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact'
 
 function App() {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'},
+    { name: 'Ada Lovelace' },
+    { name: 'Dan Abramov'},
+    { name: 'Mary Poppendieck'}
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('test')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    console.log(newName)
+    console.log(persons.includes(newName))
+    let d
+    persons.map(person => {
+      console.log(person.includes(newName))
+    })
+    setPersons(persons.concat(personObject))
+    console.log(persons)
+    setNewName('')
+      }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input type='text' value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul> 
+      {persons.map((person) => 
+          <Contact person={person} />
+        )}
+      </ul>
+      
     </div>
   )
 }
---
+++ part2/phonebook/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Contact = ({person}) => {
+    return(
+
+    <li>{person}</li>
+    )
+}
+
+export default Contact
